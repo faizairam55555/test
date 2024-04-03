e61fa716aa348d70769498f90a8779cdbbd1da03
faiza <faizairam01@gmail.com>
2024-04-03 14:53:30 +0530

    third commit

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-// Using console.log 
-console.log('Hello World');
+const http = req('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req,res) => {
+    res.statusCode=200;
+    res.setHeader('content-Type','text/plain');
+    res.end('hello wolrd');
+});
+server.listen(port,hostname,() => {
+    console.log('Server running at http://${hostname}:${port}/');
+});
